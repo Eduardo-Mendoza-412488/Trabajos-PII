5f49f15c856451ae6e5020a8bfdd37017b2111ca
unknown <412488@tecnicatura.frc.utn.edu.ar>
2024-09-23 22:35:32 -0300

    Ingreso Actividad 01

---
+++ Actividad 01/Actividad01/Datos/Utils/DataHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad01.Datos.Utils
+{
+    public class DataHelper
+    {
+        private static DataHelper _instancia;
+        public SqlConnection conexion;
+
+        private DataHelper()
+        {
+            conexion = new SqlConnection(Properties.Resources.CadenaConexion);
+        }
+
+        public static DataHelper GetInstance()
+        {
+            if (_instancia == null)
+            { _instancia = new DataHelper(); };
+
+            return _instancia;
+        }
+
+        public DataTable ExecuteSpQuery(string sp, List<ParameterSQL>? list)
+        {
+            var table = new DataTable();
+
+            try
+            {
+                conexion.Open();
+                var cm = new SqlCommand(sp, conexion);
+                cm.CommandType = System.Data.CommandType.StoredProcedure;
+                if (list != null)
+                {
+                    foreach (var param in list)
+                    {
+                        cm.Parameters.AddWithValue(param.Nombre, param.Valor);
+                    }
+                }     
+                table.Load(cm.ExecuteReader());
+
+            }
+            catch (SqlException)
+            {
+                table = null;
+            }
+            conexion.Close();
+            return table;
+        }
+
+        public int ExecuteSPDML(string sp, List<ParameterSQL>? list)
+        {
+            int rows;
+
+            try
+            {
+                conexion.Close();
+                conexion.Open();
+                var cm = new SqlCommand(sp, conexion);
+                cm.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (ParameterSQL param in list)
+                {
+                    cm.Parameters.AddWithValue(param.Nombre, param.Valor);
+                }
+                rows = cm.ExecuteNonQuery();
+
+        }
+            catch (SqlException)
+            {
+                rows = 0;
+            }
+            conexion.Close();
+            return rows;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return conexion;
+        }
+    }
+}
---
+++ Actividad 01/Actividad01/Dominio/Factura.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad01.Dominio
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public List<DetalleFactura> Detalle { get; set; }
+        public DateTime Fecha { get; set; }
+        public FormaPago FormPago { get; set; }
+        public string Cliente { get; set; }
+        public bool Activo { get; set; }
+
+
+
+        public Factura()
+        {
+            Detalle = new List<DetalleFactura>();
+        }
+
+        public override string ToString()
+        {
+            return Id + " | " + Fecha + " | " + FormPago.ToString() + " | " + Cliente + " | Activo: " + Activo;
+        }
+
+        public void AgregarArticulo(int IdArt, List<DetalleFactura> list)
+        {
+            if(list == null)
+            {
+                var oDetail1 = new DetalleFactura();
+                oDetail1.Art = new Articulo();
+                oDetail1.Art.Codigo = IdArt;
+                oDetail1.Cantidad++;
+                list.Add(oDetail1);
+            }
+            bool aux = false;
+            foreach (DetalleFactura detail in list)
+            {
+
+                if (detail.Art.Codigo == IdArt)
+                {
+                    aux = true;
+                    detail.Cantidad++;
+                    break;
+                }
+            }
+            if(aux == false)
+            {
+                var oDetail = new DetalleFactura();
+                oDetail.Art = new Articulo();
+                oDetail.Art.Codigo = IdArt;
+                oDetail.Cantidad++;
+                list.Add(oDetail);
+            }
+          
+        }
+    }
+}
